bb3b802ec08f35656eb718f6324e9e414b3defc0
Luke Carter <lukecarter@Lukes-Mac-mini.local>
2025-05-29 10:22:58 +0100

    add

--- src/components/TradeInfoModal.tsx
+++ src/components/TradeInfoModal.tsx
@@ -807,7 +807,7 @@ export default function TradeInfoModal({
                     </button>
                   </div>
                 </div>
-                {console.log('Rendering textarea with swingPlan:', swingPlan)}
+                {/* Debug info removed: console.log('Rendering textarea with swingPlan:', swingPlan) */}
                 {loadingTradeNotes ? (
                   <div className="flex items-center justify-center h-24 bg-[#1a1a1a] rounded-md border border-gray-700">
                     <svg className="animate-spin h-6 w-6 text-indigo-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
